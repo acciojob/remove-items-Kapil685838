a68b8ae38a840e93cc2ca2a7b3a23052987aa437
Kapil Chaudhary <108571992+Kapil685838@users.noreply.github.com>
2023-05-01 16:47:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const removeBtn = document.querySelector('input[type = "button"]');
+
+removeBtn.addEventListener('click', () => {
+	const colorSet = document.querySelectorAll('#colorSelect>option');
+	const color = document.getElementById('colorSelect').value;
+
+	colorSet.forEach((ele) => {
+		if(ele.innerHTML == color) {
+			document.getElementById('colorSelect').removeChild(ele);
+		}
+	});
+});
